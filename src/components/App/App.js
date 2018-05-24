6c40646c9135274cc9045f77d3a7bba1d957bd7b
Tory Dannenberg <rfdberg@gmail.com>
2018-05-24 15:01:24 -0600

    Import react in all files and export defaults

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './Welcome.js';
-import Search from './Search.js';
-import CurrentWeather from './CurrentWeather.js';
-
+import Welcome from '../Welcome/Welcome.js';
+import Search from '../Search/Search.js';
+import CurrentWeather from '../CurrentWeather/CurrentWeather.js';
+import data from '../../mock-data';
+// import SevenHour from '../SevenHour/SevenHour.js';
+// import TenDay from '../TenDay/TenDay.js'
 
 
 class App extends Component {
@@ -13,12 +15,13 @@ class App extends Component {
         <Welcome />
         <Search />
         <CurrentWeather 
-        name='Denver'
-        currTemp='79'
-        highTemp='81'
-        lowTemp='51'
-        currConditions='Partly Cloudy'
-        conditionSummary='It gonna rain'
+          city= { data.current_observation.display_location.city }
+          state= { data.current_observation.display_location.state }
+          currTemp= { data.current_observation.temp_f }
+          highTemp= { data.forecast.simpleforecast.forecastday.highTemp }
+          lowTemp='51'
+          currConditions= { data.current_observation.weather }
+          conditionSummary='It gonna rain'
         />
       </div>
     );
