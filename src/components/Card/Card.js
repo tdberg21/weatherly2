ef83560056fca5706f6ddbfc6642b45cc853c5a7
Tory Dannenberg <rfdberg@gmail.com>
2018-05-26 14:08:59 -0600

    Update cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div>
       <p> day: {props.dayName} </p>
-      <img src={props.img} alt="weather-icon"></img>
+      <img src={props.image} alt="weather-icon"></img>
       <p> high: {props.high} </p>
       <p> low: {props.low} </p>
     </div>
