c69ac939c7c9b4b0e27947b3c4271acbfb650bbd
Jesse McFadden <jessemcbrennan@gmail.com>
2018-05-30 15:43:38 -0600

    Add Search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -63,16 +63,7 @@ class App extends Component {
     } else {
       return (
       <div className="App">
-        <Search />
-        <div className="search-container">
-        <input type='text' placeholder='Enter a City and State or a Zip Code.' onChange={(event) => {
-          this.setState({
-            location: event.target.value
-          })
-        }}
-        />
-        <button className="submitButton"onClick={this.fetchData}>Submit</button>
-          </div>
+        <Search fetchData={this.fetchData} />
         <CurrentWeather
           city={this.state.city}
           state={this.state.state}
