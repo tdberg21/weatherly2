7c7c7c5c7b4d50f91ee4bb9d5e1d638b4c59902d
Jesse McFadden <jessemcbrennan@gmail.com>
2018-05-30 10:24:19 -0600

    Add app test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import { shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App default state', () => {
+  it('should have a default location of an empty string', () => {
+    renderedApp = shallow(<App />)
+
+    const expectedLocation = '';
+    const actualLocation = renderedApp.state('location');
+
+    expect(actualLocation).toEqual(expectedLocation);
+    })
 });
 
