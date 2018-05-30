6c7e5bec040fb3dcd73cc86748797ed20ef2b450
Jesse McFadden <jessemcbrennan@gmail.com>
2018-05-29 21:09:18 -0600

    Add styling to cards

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,18 @@
-// import React from 'react';
-// import apiKey from './src/apiKey.js'
+import React from 'react';
+import apiKey from './src/apiKey.js'
 
 
-// function fetchData(state, city) {
-//   const url = `http://api.wunderground.com/api/${apiKey}/conditions/q/${state}/${city}.json`;
-//     const promise = fetch(url)  
-//                     .then(data => data.json());
-//     return promise;
-// }
+function fetchData(state, city) {
+  const url = `http://api.wunderground.com/api/${apiKey}/conditions/q/${state}/${city}.json`;
+    const promise = fetch(url)  
+                    .then(data => data.json());
 
-// export default fetchData;
+    return promise;
+}
 
-// // const data = fetch('http://api.wunderground.com/api/${apiKey}/conditions/q/${searchState}/${searchCity}.json')
-// //   .then(data => data.json())
-// //   .then(data => console.log(data))
-// //   .catch(err => console.log('data error'))
+export default fetchData;
+
+// const data = fetch('http://api.wunderground.com/api/${apiKey}/conditions/q/${searchState}/${searchCity}.json')
+//   .then(data => data.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.log('data error'))
